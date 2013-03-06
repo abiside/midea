ff94273a101d5f6c69cde203e3888a754887f77d
Carlos Guillermo Gomez Zayas <camarongomez@hotmail.com>
2013-03-05 23:11:45 -0700

    Background effects fixes

--- application/views/home.php
+++ application/views/home.php
@@ -5,50 +5,46 @@
 
 <div id="slider">
 
-	<div class="ls-layer" id="slide1" >
-		<img class="ls-bg sliderbg" src="<?=base_url()?>media/img/slider/slider-bg-3.jpg">
+	<div class="ls-layer slidesbg" id="slide1" >
 		<div class="ls-s1 slide-text firefix" style="width:100%;top:280px;">Mejorando tu estilo de vida</div>
 	</div>
 
-	<div class="ls-layer" id="slide2" style="">
-		<img class="ls-bg sliderbg" src="<?=base_url()?>media/img/slider/slider-bg-2.jpg">
-
+	<div class="ls-layer slidesbg" id="slide2" style="">
 		<div class="ls-s1 slide-text" style="width:100%;top:100px;">Mejorando tu estilo de vida</div>
 		<div class="ls-s2 slide-img" style="top:180px;"><img src="<?=base_url()?>media/img/slider/slider-img-2.png"></div>
 	</div>
 
-	<div class="ls-layer" id="slide3" style="">
-		<img class="ls-bg sliderbg" src="<?=base_url()?>media/img/slider/slider-bg-1.jpg">
-
+	<div class="ls-layer slidesbg" id="slide3" style="">
 		<div class="ls-s1 slide-text" style="width:100%;top:80px;">Mejorando tu estilo de vida</div>
 		<div class="ls-s2 slide-img" style="top:190px;"><img src="<?=base_url()?>media/img/slider/slider-img-1.png"></div>
 	</div>
 
 </div>
 
-<div class="limited">
-	<div id="big-buttons">
-		<div class="button">
-			<div class="title">Sobre Nosotros</div>
-			<div class="icon"><img src="<?=base_url()?>media/img/lib/icon-nosotros.png"></div>
-			<div class="text">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio</div>
-		</div>
-		<div class="button">
-			<div class="title">Productos</div>
-			<div class="icon"><img src="<?=base_url()?>media/img/lib/icon-productos.png"></div>
-			<div class="text">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio</div>
-		</div>
-		<div class="button last">
-			<div class="title">Distribuidor</div>
-			<div class="icon"><img src="<?=base_url()?>media/img/lib/icon-distribuidor.png"></div>
-			<div class="text">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio</div>
+<div class="homeframe">
+	<div class="limited">
+		<div id="big-buttons">
+			<div class="button">
+				<div class="title">Sobre Nosotros</div>
+				<div class="icon"><img src="<?=base_url()?>media/img/lib/icon-nosotros.png"></div>
+				<div class="text">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio</div>
+			</div>
+			<div class="button">
+				<div class="title">Productos</div>
+				<div class="icon"><img src="<?=base_url()?>media/img/lib/icon-productos.png"></div>
+				<div class="text">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio</div>
+			</div>
+			<div class="button last">
+				<div class="title">Distribuidor</div>
+				<div class="icon"><img src="<?=base_url()?>media/img/lib/icon-distribuidor.png"></div>
+				<div class="text">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio</div>
+			</div>
 		</div>
 	</div>
-</div>
-
+</div> 
 
 <div id="soporte-frame">
-	<div class="shadow"></div>
+	<div class="shadowa"></div>
 
 	<div class="limited">
 		<div class="twocolumns">

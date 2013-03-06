bf484e168f8efb94f1fbe1a1a1c542b1b3588195
Abimael Espinoza <abimael.espinoza@holdasociados.com>
2013-03-06 00:04:45 -0600

    productos

---
+++ application/views/productos.php
@@ -0,0 +1,20 @@
+<div id="imagearea"></div>
+
+<div id="nosotroscontenido">
+	<h1>Nosotros</h1>
+	<div id="descripcion">
+		Midea es una empresa fundada en Shunde, Guangdong, capital de los electrodomésticos en China.<br><br>
+		Por más de 40 años, Midea ha colaborado para mejorar la calidad de vida de las personas, mediante la experiencia, nuevas tecnologías y la innovación continua.<br><br>
+		Midea se ha desarrollado desde una empresa pequeña a ser una empresa de clase mundial en línea blanca, ofreciendo productos de alta calidad a precios muy competitivos. Sus productos principales incluyen minisplits, aire acondicionado comercial, lavadoras, refrigeradores, dispensadores de agua, hornos de microondas, lavavajillas, ventiladores eléctricos, calentadores eléctricos, calentadores de agua, hornos eléctricos, aspiradoras, enseres domésticos, así como otros productos de apoyo, tales como compresores, motores eléctricos, magnetrones y transformadores.<br><br>
+		En 2008 llega a México para comercializar sus productos de la línea de aire acondicionado.
+	</div>
+	<div id="ceoarea">
+		<div id="ceoquote" class="didot">
+		“ Por más de 40 años, Midea ha colaborado para mejorar la calidad de vida de las personas, mediante la experiencia, 	nuevas tecnologías y la innovación continua.”
+			<div id="ceoname"> 
+				- <span> JACKIE CHAN </span>  CEO MIDEA
+			</div>
+		</div>
+		<div id="ceoimg"></div>
+	</div>
+</div>

a3090e6232bf8f932f092101fea6c891bd8518d2
Carlos Guillermo Gomez Zayas <camarongomez@hotmail.com>
2013-03-07 22:23:16 -0700

    Efecto zoom home

--- application/views/home.php
+++ application/views/home.php
@@ -26,17 +26,17 @@
 		<div id="big-buttons">
 			<div class="button">
 				<div class="title">Sobre Nosotros</div>
-				<div class="icon"><img src="<?=base_url()?>media/img/lib/icon-nosotros.png"></div>
+				<div class="icon"><img class="zoomico" src="<?=base_url()?>media/img/lib/icon-nosotros.png"><img src="<?=base_url()?>media/img/lib/ironcircle.png"></div>
 				<div class="text">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio</div>
 			</div>
 			<div class="button">
 				<div class="title">Productos</div>
-				<div class="icon"><img src="<?=base_url()?>media/img/lib/icon-productos.png"></div>
+				<div class="icon"><img class="zoomico" src="<?=base_url()?>media/img/lib/icon-productos.png"><img src="<?=base_url()?>media/img/lib/ironcircle.png"></div>
 				<div class="text">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio</div>
 			</div>
 			<div class="button last">
 				<div class="title">Distribuidor</div>
-				<div class="icon"><img src="<?=base_url()?>media/img/lib/icon-distribuidor.png"></div>
+				<div class="icon"><img class="zoomico" src="<?=base_url()?>media/img/lib/icon-distribuidor.png"><img src="<?=base_url()?>media/img/lib/ironcircle.png"></div>
 				<div class="text">Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio</div>
 			</div>
 		</div>

c3fe47c6bdbe09d3503495ff9a168a6a57dd5eef
Abimael Espinoza <abimael.espinoza@holdasociados.com>
2013-04-19 17:54:08 -0500

    codificacion

--- application/views/layout.php
+++ application/views/layout.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
  <head>  
-<meta http-equiv="Content-Type" content="text/html; charset=utf8" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
 <title><?=$page_title?> - Midea México - Aires acondicionados</title>
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />   
 <link rel="stylesheet" href="<?=base_url()?>media/css/styles.css" />
